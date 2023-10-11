f1657e3ac4fedc27a051b7bf814fc49f24dea4a5
Vanelton <145939472+Vanelton@users.noreply.github.com>
2023-10-11 13:45:24 -0300

    Create language.js

---
+++ language.js
@@ -0,0 +1,73 @@
+// ARQUIVO DE LINGUAGEM
+
+const language_words = {
+    "us":[
+        "From: ", // 0
+        "Loading...", // 1
+        "Today, 10/11/2023", // 2
+        " it's a special day because it's a special person's birthday,", // 3
+        "\r\nVEEEERY", // 4
+        " special.", // 5
+        "It's her birthday, the woman", // 6
+        "Wonderful", // 7
+        "Smart", // 8
+        "Dedicated", // 9
+        "Fun", // 10
+        "Funny", // 11
+        "Eagle Interpreter", // 12
+        "Ariana Grande's biggest fan", // 13
+        "After's biggest hater", // 14
+        "The owner of a beautiful BMW", // 15
+        "Owner of 'EEEEEEHHHHRRR MY FRIEND'", // 16
+        "Her name?", // 17
+        "Valesca", // 18
+        "HAPPY BIRTHDAY", // 19
+        "Do u remember?", // 20
+        "I remember!", // 21
+        "All moments", // 22
+        "Cinema", // 23
+        "Beeeeeeaach", // 24
+        "Fotão", // 25
+        "Did u choose one?", // 26
+        "<3", // 27
+        "Orange", // 28
+        "Hi, do u want McFries?", // 29
+        "'Why re they in our chair?'", // 30
+        "U re too special for a normal congratulation", // 31
+    ],
+
+    "br":[
+        "De: ", // 0
+        "Carregando...", // 1
+        "Hoje, 11/10/2023", // 2
+        " é um dia especial, pois é o aniversário de uma pessoa muito,", // 3
+        "\r\nMUUUUITO", // 4
+        " especial.", // 5
+        "É o aniversário dela, uma pessoa", // 6
+        "Maravilhosa", // 7
+        "Inteligente", // 8
+        "Dedicada", // 9
+        "Divertida", // 10
+        "Engraçada", // 11
+        "Interprete de Águias", // 12
+        "Da maior fã da Ariana Grande", // 13
+        "Da maior hater de After", // 14
+        "Da dona de uma belissima BMW", // 15
+        "Da dona do 'EEEEEEHHHHRRR MEU AMIGO'", // 16
+        "O nome dessa pessoa incrível?", // 17
+        "Valesca", // 18
+        "FELIZ ANIVERSÁRIO", // 19
+        "Você se lembra?", // 20
+        "Eu me lembro!", // 21
+        "De todos os momentos", // 22
+        "O Cinema", // 23
+        "Praiaaaaaaaa", // 24
+        "Fotão", // 25
+        "Qual escolher?", // 26
+        "<3", // 27
+        "Levou o Orangeee", // 28
+        "Oii, quer batata?", // 29
+        "Calma, podemos ficar aqui e eles lá", // 30
+        "Você é especial demais para um parabéns normal", // 31
+    ],
+}
